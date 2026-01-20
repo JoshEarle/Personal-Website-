63f310a0e6f018e502a42c7f373f0edf4edc58dc
Joshua Earle <josh@outlit.ai>
2026-01-19 21:49:08 -0800

    Update Outlit SDK: add trackPageviews and update public key

--- components/OutlitProvider.tsx
+++ components/OutlitProvider.tsx
@@ -4,7 +4,7 @@ import { OutlitProvider as OutlitProviderComponent } from "@outlit/browser/react
 
 export function OutlitProvider({ children }: { children: React.ReactNode }) {
   return (
-    <OutlitProviderComponent publicKey="pk_51sW2wA-YWQQCZ2aFeb3EVG7kz">
+    <OutlitProviderComponent publicKey="pk_51sW2wA-YWQQCZ2aFeb3EVG7kzx0Jqbc" trackPageviews>
       {children}
     </OutlitProviderComponent>
   );
